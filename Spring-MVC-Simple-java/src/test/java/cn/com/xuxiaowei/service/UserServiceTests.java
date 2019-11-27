57b54d7f44a5e84ef123c27d1fe1f2d507042353
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2019-11-28 00:16:25 +0800

    修正优化

--- Spring-MVC-Simple-java/src/test/java/cn/com/xuxiaowei/service/UserServiceTests.java
+++ Spring-MVC-Simple-java/src/test/java/cn/com/xuxiaowei/service/UserServiceTests.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 /**
@@ -17,7 +17,7 @@ import org.springframework.test.context.web.WebAppConfiguration;
  * @see <a href="https://spring.io/blog/2014/05/23/preview-spring-security-test-web-security">Preview Spring Security Test: Web Security</a>
  * @since 0.0.1
  */
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @ContextConfiguration(classes = {SpringContextConfiguration.class})
 @WebAppConfiguration
 public class UserServiceTests {
@@ -28,7 +28,7 @@ public class UserServiceTests {
     @Test
     public void selectByUsername() {
         User user = userService.selectByUsername("徐晓伟");
-        System.out.println(userService);
+        System.out.println(user);
     }
 
 }
