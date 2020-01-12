f21445015750e07d2946fd8fd60e539412404a9c
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-01-12 19:58:53 +0800

    修正方法名

--- Spring-MVC-Simple-xml/src/main/java/cn/com/xuxiaowei/controller/IndexController.java
+++ Spring-MVC-Simple-xml/src/main/java/cn/com/xuxiaowei/controller/IndexController.java
@@ -58,7 +58,7 @@ public class IndexController {
 
         logger.debug(this.getClass() + "debug 日志测试。");
 
-        User user = userService.selectByUsername("徐晓伟");
+        User user = userService.getByUsername("徐晓伟");
 
         logger.debug(user);
 
