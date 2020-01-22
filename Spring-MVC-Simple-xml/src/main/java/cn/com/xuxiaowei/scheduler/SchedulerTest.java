9ef933da5f18275d40f8881f6fb8631e758165b1
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-01-22 09:02:14 +0800

    定时器 测试类

---
+++ Spring-MVC-Simple-xml/src/main/java/cn/com/xuxiaowei/scheduler/SchedulerTest.java
@@ -0,0 +1,43 @@
+package cn.com.xuxiaowei.scheduler;
+
+import cn.com.xuxiaowei.properties.JdbcProperties;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * 定时器 测试类
+ * <p>
+ * 由于 {@link Scheduled} 使用了 Java 8 的 @Repeatable 注解，构建编译时，使用 JDK 6 可能会出现警告
+ *
+ * @author xuxiaowei
+ * @see <a href="https://docs.oracle.com/javase/8/docs/api/java/lang/annotation/Repeatable.html">Java 8 @Repeatable</a>
+ * @since 0.0.1
+ */
+@Slf4j
+@Component
+public class SchedulerTest {
+
+    /**
+     * 用于测试 {@link Autowired} 使用注入
+     */
+    private JdbcProperties jdbcProperties;
+
+    /**
+     * 标准注入
+     */
+    @Autowired
+    public void setJdbcProperties(JdbcProperties jdbcProperties) {
+        this.jdbcProperties = jdbcProperties;
+    }
+
+    /**
+     * 每 5 秒执行一次
+     */
+    @Scheduled(cron = "0/5 * * * * *")
+    public void job() {
+        log.info(jdbcProperties.toString());
+    }
+
+}
