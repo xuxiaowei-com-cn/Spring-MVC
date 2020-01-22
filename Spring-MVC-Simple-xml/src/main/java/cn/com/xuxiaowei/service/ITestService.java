89e5e77b7abe357f5aa002004fa837c0b1b4c48d
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-01-22 13:42:38 +0800

    测试类

---
+++ Spring-MVC-Simple-xml/src/main/java/cn/com/xuxiaowei/service/ITestService.java
@@ -0,0 +1,18 @@
+package cn.com.xuxiaowei.service;
+
+/**
+ * 测试 服务层
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+public interface ITestService {
+
+    /**
+     * 测试方法
+     *
+     * @return 返回测试内容
+     */
+    String hi();
+
+}
