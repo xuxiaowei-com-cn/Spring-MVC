db0b274e898823f37c21c77cefbd6cfbe96d1fc4
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-01-27 18:16:00 +0800

    加载 Redis 属性文件

---
+++ Spring-MVC-Simple-java/src/main/java/cn/com/xuxiaowei/properties/RedisProperties.java
@@ -0,0 +1,32 @@
+package cn.com.xuxiaowei.properties;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * 加载 Redis 属性文件
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+@Data
+@Configuration
+@PropertySource(value = {"classpath:redis.properties"})
+public class RedisProperties {
+
+    @Value("${redis.host-name}")
+    private String hostName;
+
+    @Value("${redis.port}")
+    private int port;
+
+    @Value("${redis.password}")
+    private String password;
+
+
+    @Value("${redis.database}")
+    private int database;
+
+}
