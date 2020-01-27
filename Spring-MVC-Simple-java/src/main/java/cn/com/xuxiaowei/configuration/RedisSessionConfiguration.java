68ec33838507b9dfe486e5612c4ba8e7bc24ef25
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-01-27 18:16:28 +0800

    Redis 属性

--- Spring-MVC-Simple-java/src/main/java/cn/com/xuxiaowei/configuration/RedisSessionConfiguration.java
+++ Spring-MVC-Simple-java/src/main/java/cn/com/xuxiaowei/configuration/RedisSessionConfiguration.java
@@ -1,10 +1,12 @@
 package cn.com.xuxiaowei.configuration;
 
+import cn.com.xuxiaowei.properties.RedisProperties;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -31,6 +33,16 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 public class RedisSessionConfiguration {
 
     /**
+     * Redis 属性
+     */
+    private RedisProperties redisProperties;
+
+    @Autowired
+    public void setRedisProperties(RedisProperties redisProperties) {
+        this.redisProperties = redisProperties;
+    }
+
+    /**
      * 配置 Lettuce Redis 连接器
      */
     @Bean
@@ -39,9 +51,10 @@ public class RedisSessionConfiguration {
         // hostName：默认值：localhost
         // port：默认值：6379
         // password：默认值：
-        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory();
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(
+                redisProperties.getHostName(), redisProperties.getPort());
 
-        lettuceConnectionFactory.setDatabase(1);
+        lettuceConnectionFactory.setDatabase(redisProperties.getDatabase());
 
         return lettuceConnectionFactory;
     }
