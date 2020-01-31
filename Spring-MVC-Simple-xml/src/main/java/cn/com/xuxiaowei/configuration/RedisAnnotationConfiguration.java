9985c97815dbce8810f3bed1b1092a823c853160
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-02-01 00:58:11 +0800

    开启 Redis 注解

---
+++ Spring-MVC-Simple-xml/src/main/java/cn/com/xuxiaowei/configuration/RedisAnnotationConfiguration.java
@@ -0,0 +1,17 @@
+package cn.com.xuxiaowei.configuration;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 开启 Redis 注解
+ *
+ * @author xuxiaowei
+ * @see <a href="https://docs.spring.io/spring-session/docs/current/reference/html5/#httpsession-spring-configuration">httpsession-spring-configuration</a>
+ * @since 0.0.1
+ */
+@Configuration
+@EnableCaching
+public class RedisAnnotationConfiguration {
+
+}
