9bda7a4554708a7e62e9b01cfc5a7b94aa03d4a3
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-02-02 20:28:37 +0800

    Redis 测试类

--- Spring-MVC-Simple-xml/src/test/java/cn/com/xuxiaowei/redis/RedisTests.java
+++ Spring-MVC-Simple-xml/src/test/java/cn/com/xuxiaowei/redis/RedisTests.java
@@ -1,11 +1,18 @@
 package cn.com.xuxiaowei.redis;
 
+import cn.com.xuxiaowei.entity.User;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import redis.clients.jedis.Jedis;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Redis 测试类
@@ -15,8 +22,38 @@ import redis.clients.jedis.Jedis;
  */
 @Slf4j
 @RunWith(SpringRunner.class)
-@ContextConfiguration(value = {"classpath:/spring-context.xml"})
+@ContextConfiguration(value = {"classpath:spring-context.xml", "classpath:redis-spring.xml"})
 public class RedisTests {
+
+    @Resource
+    private RedisTemplate<String, User> redisTemplate;
+
+    /**
+     * 操作字符串 {@link RedisTemplate#opsForValue()}
+     *
+     * @see RedisTemplate#opsForHash() 操作有序 Hash
+     * @see RedisTemplate#opsForList() 操作有序 {@link List}
+     * @see RedisTemplate#opsForSet() 操作有序 {@link Set}
+     * @see RedisTemplate#opsForZSet() 操作有序 Set
+     */
+    @Test
+    public void opsForValue() {
+
+        String username = "xxw";
+
+        ValueOperations<String, User> valueOperations = redisTemplate.opsForValue();
+
+        User user = new User();
+        user.setUserId(666L);
+        user.setUsername(username);
+        user.setPassword("xxw123");
+
+        valueOperations.set(username, user);
+
+        User userRedis = valueOperations.get(username);
+
+        log.debug(String.valueOf(userRedis));
+    }
 
     @Test
     public void jedisLocalhost() {
