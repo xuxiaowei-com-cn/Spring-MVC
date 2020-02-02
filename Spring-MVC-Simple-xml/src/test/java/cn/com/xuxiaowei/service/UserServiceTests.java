03688f05ce542f46fb18e0068f1d7593565bfddc
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-02-02 20:07:47 +0800

    补全 @ContextConfiguration

--- Spring-MVC-Simple-xml/src/test/java/cn/com/xuxiaowei/service/UserServiceTests.java
+++ Spring-MVC-Simple-xml/src/test/java/cn/com/xuxiaowei/service/UserServiceTests.java
@@ -18,7 +18,7 @@ import java.util.UUID;
  */
 @Slf4j
 @RunWith(SpringRunner.class)
-@ContextConfiguration(value = {"classpath:/spring-context.xml"})
+@ContextConfiguration(value = {"classpath:spring-context.xml", "classpath:redis-spring.xml"})
 public class UserServiceTests {
 
     @Resource
