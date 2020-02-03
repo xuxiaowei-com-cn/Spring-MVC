5c430e9386618306ac7187fa01e16b40dcba7347
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-02-04 02:53:28 +0800

    补全

--- Spring-MVC-Simple-java/src/main/java/cn/com/xuxiaowei/controller/IndexController.java
+++ Spring-MVC-Simple-java/src/main/java/cn/com/xuxiaowei/controller/IndexController.java
@@ -24,7 +24,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 
 import javax.servlet.http.HttpServletRequest;
