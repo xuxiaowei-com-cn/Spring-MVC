bf5892983e7aa558ca4c975c2869d7005cba7c25
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-02-04 17:30:27 +0800

    登录 Controller

---
+++ Spring-MVC-Simple-java/src/main/java/cn/com/xuxiaowei/controller/LoginController.java
@@ -0,0 +1,34 @@
+package cn.com.xuxiaowei.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static cn.com.xuxiaowei.util.Constants.LOGIN;
+
+/**
+ * 登录 Controller
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+@Controller
+public class LoginController {
+
+    /**
+     * 登录页面
+     *
+     * @see RequestMethod#GET
+     */
+    @GetMapping(LOGIN)
+    public String login(HttpServletRequest request, HttpServletResponse response, Model model) {
+
+        return "html/login";
+    }
+
+
+}
