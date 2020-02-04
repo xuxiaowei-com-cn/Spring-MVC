79568f7622c85e43d4e58c7b46b32f0d1c2030dc
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-02-05 03:13:26 +0800

    注释

--- Spring-MVC-Simple-java/src/main/java/cn/com/xuxiaowei/controller/IndexJsfController.java
+++ Spring-MVC-Simple-java/src/main/java/cn/com/xuxiaowei/controller/IndexJsfController.java
@@ -3,20 +3,22 @@ package cn.com.xuxiaowei.controller;
 import cn.com.xuxiaowei.service.ITestService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import java.io.Serializable;
 import java.util.Date;
 
 /**
  * JSF Controller
  *
- * <code>@ManagedBean</code>
- * <code>@SessionScoped</code>
- *
  * @author xuxiaowei
  * @see Serializable 用于 Session 共享的序列化
+ * @see ManagedBean  托管的 {@link Bean}，可自定义 {@link Bean} 的名字
+ * @see SessionScoped
  * @since 0.0.1
  */
 @Slf4j
