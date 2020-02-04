349637a5ae200b99a80a41a14809fc8f88e98a20
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-02-04 23:22:50 +0800

    注销 RestController

---
+++ Spring-MVC-Simple-java/src/main/java/cn/com/xuxiaowei/controller/LogoutRestController.java
@@ -0,0 +1,47 @@
+package cn.com.xuxiaowei.controller;
+
+import cn.com.xuxiaowei.configuration.WebMvcConfigurerConfiguration;
+import cn.com.xuxiaowei.util.Constants;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+import static cn.com.xuxiaowei.util.Constants.*;
+
+/**
+ * 注销 RestController
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+@RestController
+public class LogoutRestController {
+
+    /**
+     * 注销登录成功
+     *
+     * @see Constants#LOGOUT_SUCCESS
+     * @see Constants#LOGOUT_SUCCESS_JSON
+     * @see RequestMethod#POST
+     * @see WebMvcConfigurerConfiguration#configureContentNegotiation(ContentNegotiationConfigurer)
+     */
+    @PostMapping(LOGOUT_SUCCESS)
+    public Map<String, Object> logoutSuccess(HttpServletRequest request, HttpServletResponse response) {
+
+        Map<String, Object> map = new HashMap<>(4);
+        Map<String, Object> data = new HashMap<>(4);
+        map.put("data", data);
+
+        map.put(CODE, CODE_OK);
+        map.put(MSG, "注销登录成功");
+
+        return map;
+    }
+
+}
