ec0ca14e48d7e6a193c4615cf1041b751e588b1e
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-02-05 01:52:36 +0800

    注销登录

---
+++ Spring-MVC-Simple-java/src/main/webapp/static/js/logout.js
@@ -0,0 +1,40 @@
+/**
+ * 注销登录
+ */
+function logout(url) {
+
+    const header = $("meta[name='_csrf_header']").attr("content");
+    const token = $("meta[name='_csrf']").attr("content");
+
+    console.log(header, token);
+
+    $.ajax({
+        url: url,
+        method: "post",
+        dataType: 'json',
+        data: {},
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader(header, token);
+        },
+        success: function (result) {
+            console.log("result", result);
+
+            const code = result.code;
+            const msg = result.msg;
+
+            if (code === 0) {
+                alert(msg);
+                location.reload();
+            } else if (code === 1) {
+                alert(msg);
+            } else {
+                alert("响应 Code 异常：" + result);
+            }
+
+        },
+        error: function (error) {
+            console.log("error", error);
+        }
+    })
+
+}
