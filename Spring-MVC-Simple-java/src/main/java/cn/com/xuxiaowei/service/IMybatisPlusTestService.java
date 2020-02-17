92e0c3fd3136f63d369e81007f4e0a473934b01e
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-02-18 01:31:59 +0800

    MyBatis Plus 测试表

---
+++ Spring-MVC-Simple-java/src/main/java/cn/com/xuxiaowei/service/IMybatisPlusTestService.java
@@ -0,0 +1,16 @@
+package cn.com.xuxiaowei.service;
+
+import cn.com.xuxiaowei.entity.MybatisPlusTest;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * MyBatis Plus 测试表 服务类
+ * </p>
+ *
+ * @author 徐晓伟
+ * @since 2020-02-18
+ */
+public interface IMybatisPlusTestService extends IService<MybatisPlusTest> {
+
+}
