6d38c52721a535536cf3fdfa6193a82189564aba
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-02-18 01:21:27 +0800

    测试 MyBatis Plus

---
+++ Spring-MVC-Simple-xml/src/main/java/cn/com/xuxiaowei/mapper/MybatisPlusTestMapper.java
@@ -0,0 +1,16 @@
+package cn.com.xuxiaowei.mapper;
+
+import cn.com.xuxiaowei.entity.MybatisPlusTest;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * MyBatis Plus 测试表 Mapper 接口
+ * </p>
+ *
+ * @author 徐晓伟
+ * @since 2020-02-18
+ */
+public interface MybatisPlusTestMapper extends BaseMapper<MybatisPlusTest> {
+
+}
---
+++ Spring-MVC-Simple-xml/src/main/java/cn/com/xuxiaowei/service/impl/MybatisPlusTestServiceImpl.java
@@ -0,0 +1,20 @@
+package cn.com.xuxiaowei.service.impl;
+
+import cn.com.xuxiaowei.entity.MybatisPlusTest;
+import cn.com.xuxiaowei.mapper.MybatisPlusTestMapper;
+import cn.com.xuxiaowei.service.IMybatisPlusTestService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * MyBatis Plus 测试表 服务实现类
+ * </p>
+ *
+ * @author 徐晓伟
+ * @since 2020-02-18
+ */
+@Service
+public class MybatisPlusTestServiceImpl extends ServiceImpl<MybatisPlusTestMapper, MybatisPlusTest> implements IMybatisPlusTestService {
+
+}
