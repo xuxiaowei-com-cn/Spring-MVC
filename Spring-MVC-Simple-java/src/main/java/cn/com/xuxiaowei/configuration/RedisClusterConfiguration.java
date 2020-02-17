a84222574be8ca61f91de59df465fdabe750d209
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-02-17 22:53:59 +0800

    Redis 集群配置

---
+++ Spring-MVC-Simple-java/src/main/java/cn/com/xuxiaowei/configuration/RedisClusterConfiguration.java
@@ -0,0 +1,62 @@
+package cn.com.xuxiaowei.configuration;
+
+import cn.com.xuxiaowei.properties.RedisClusterProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisNode;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+
+/**
+ * Redis 集群配置
+ * <p>
+ * {@link RedisSingleConfiguration} 与 {@link RedisClusterConfiguration} 使用一个
+ * <p>
+ * {@link Configuration}
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+public class RedisClusterConfiguration {
+
+    /**
+     * Redis 集群属性文件
+     */
+    private RedisClusterProperties redisClusterProperties;
+
+    /**
+     * {@link Autowired}
+     */
+    public void setRedisClusterProperties(RedisClusterProperties redisClusterProperties) {
+        this.redisClusterProperties = redisClusterProperties;
+    }
+
+    /**
+     * 配置 Jedis Redis 集群连接器
+     * <p>
+     * {@link Bean}
+     */
+    public JedisConnectionFactory jedisConnectionFactory() {
+
+        // 类名重复，使用全限定名
+        org.springframework.data.redis.connection.RedisClusterConfiguration redisClusterConfiguration =
+                new org.springframework.data.redis.connection.RedisClusterConfiguration();
+
+        RedisNode redisNode1 = new RedisNode(redisClusterProperties.getHost1(), redisClusterProperties.getPort1());
+        RedisNode redisNode2 = new RedisNode(redisClusterProperties.getHost2(), redisClusterProperties.getPort2());
+        RedisNode redisNode3 = new RedisNode(redisClusterProperties.getHost3(), redisClusterProperties.getPort3());
+        RedisNode redisNode4 = new RedisNode(redisClusterProperties.getHost4(), redisClusterProperties.getPort4());
+        RedisNode redisNode5 = new RedisNode(redisClusterProperties.getHost5(), redisClusterProperties.getPort5());
+        RedisNode redisNode6 = new RedisNode(redisClusterProperties.getHost6(), redisClusterProperties.getPort6());
+
+        redisClusterConfiguration.addClusterNode(redisNode1);
+        redisClusterConfiguration.addClusterNode(redisNode2);
+        redisClusterConfiguration.addClusterNode(redisNode3);
+        redisClusterConfiguration.addClusterNode(redisNode4);
+        redisClusterConfiguration.addClusterNode(redisNode5);
+        redisClusterConfiguration.addClusterNode(redisNode6);
+
+        return new JedisConnectionFactory(redisClusterConfiguration);
+    }
+
+}
