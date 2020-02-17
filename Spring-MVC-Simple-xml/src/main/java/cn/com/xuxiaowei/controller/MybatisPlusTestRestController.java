c2f1448f290871e8969445d44814928e0ac513e8
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-02-18 02:49:47 +0800

    MyBatis Plus 测试

---
+++ Spring-MVC-Simple-xml/src/main/java/cn/com/xuxiaowei/controller/MybatisPlusTestRestController.java
@@ -0,0 +1,20 @@
+package cn.com.xuxiaowei.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * MyBatis Plus 测试表 前端控制器
+ * </p>
+ *
+ * @author 徐晓伟
+ * @since 2020-02-18
+ */
+@RestController
+@RequestMapping("/mybatis-plus-test")
+public class MybatisPlusTestRestController {
+
+}
+
---
+++ Spring-MVC-Simple-xml/src/main/java/cn/com/xuxiaowei/entity/MybatisPlusTest.java
@@ -0,0 +1,44 @@
+package cn.com.xuxiaowei.entity;
+
+import com.baomidou.mybatisplus.annotations.TableField;
+import com.baomidou.mybatisplus.annotations.TableId;
+import com.baomidou.mybatisplus.annotations.TableName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+/**
+ * <p>
+ * MyBatis Plus 测试表
+ * </p>
+ *
+ * @author 徐晓伟
+ * @since 2020-02-18
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@TableName("mybatis_plus_test")
+public class MybatisPlusTest implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId("test_id")
+    private Long testId;
+
+    @TableField("test1")
+    private String test1;
+
+    @TableField("test2")
+    private String test2;
+
+
+    public static final String TEST_ID = "test_id";
+
+    public static final String TEST1 = "test1";
+
+    public static final String TEST2 = "test2";
+
+}
