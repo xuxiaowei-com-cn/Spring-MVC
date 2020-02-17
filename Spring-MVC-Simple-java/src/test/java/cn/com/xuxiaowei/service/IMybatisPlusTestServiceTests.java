292a01025d8d572cbd1b2aa54d6eae07e12b9b5b
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-02-18 02:57:26 +0800

    MyBatis Plus 测试

---
+++ Spring-MVC-Simple-java/src/test/java/cn/com/xuxiaowei/service/IMybatisPlusTestServiceTests.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cn.com.xuxiaowei.service;
+
+import cn.com.xuxiaowei.configuration.SpringContextConfiguration;
+import cn.com.xuxiaowei.entity.MybatisPlusTest;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+/**
+ * MyBatis Plus 测试表 服务类 测试类
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+@Slf4j
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {SpringContextConfiguration.class})
+@WebAppConfiguration
+public class IMybatisPlusTestServiceTests {
+
+    @Autowired
+    private IMybatisPlusTestService iMybatisPlusTestService;
+
+    @Test
+    public void selectById() {
+        MybatisPlusTest byId = iMybatisPlusTestService.selectById(1);
+        log.info(String.valueOf(byId));
+    }
+
+}
