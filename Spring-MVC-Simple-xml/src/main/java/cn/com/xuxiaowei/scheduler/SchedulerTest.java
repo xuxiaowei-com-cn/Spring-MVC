de69359614f8b8d7422811395eb9ebee99727a40
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-02-25 22:00:49 +0800

    注释

--- Spring-MVC-Simple-xml/src/main/java/cn/com/xuxiaowei/scheduler/SchedulerTest.java
+++ Spring-MVC-Simple-xml/src/main/java/cn/com/xuxiaowei/scheduler/SchedulerTest.java
@@ -18,13 +18,10 @@ package cn.com.xuxiaowei.scheduler;
 import cn.com.xuxiaowei.properties.JdbcProperties;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 /**
  * 定时器 测试类
- * <p>
- * 由于 {@link Scheduled} 使用了 Java 8 的 @Repeatable 注解，构建编译时，使用 JDK 6 可能会出现警告
  *
  * @author xuxiaowei
  * @see <a href="https://docs.oracle.com/javase/8/docs/api/java/lang/annotation/Repeatable.html">Java 8 @Repeatable</a>
