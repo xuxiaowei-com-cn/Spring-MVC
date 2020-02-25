c811a0dbfe10539b8d0311abd76c48f3717e337a
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-02-25 20:59:56 +0800

    测试 HttpServlet 配置

--- Spring-MVC-Simple-xml/src/main/java/cn/com/xuxiaowei/servlet/HttpServletTest.java
+++ Spring-MVC-Simple-xml/src/main/java/cn/com/xuxiaowei/servlet/HttpServletTest.java
@@ -15,8 +15,9 @@
  */
 package cn.com.xuxiaowei.servlet;
 
+import lombok.extern.slf4j.Slf4j;
+
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -32,7 +33,7 @@ import static cn.com.xuxiaowei.util.Constants.*;
  * @author xuxiaowei
  * @since 0.0.1
  */
-@WebServlet(urlPatterns = {"/httpServletTest"})
+@Slf4j
 public class HttpServletTest extends HttpServlet {
 
     @Override
