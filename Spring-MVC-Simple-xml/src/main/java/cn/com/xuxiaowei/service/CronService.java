643f0a7530975c52fc381b1ed156915bed798231
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-02-25 22:01:47 +0800

    定时器

---
+++ Spring-MVC-Simple-xml/src/main/java/cn/com/xuxiaowei/service/CronService.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cn.com.xuxiaowei.service;
+
+import cn.com.xuxiaowei.entity.Cron;
+
+import java.util.List;
+
+/**
+ * 定时器 服务层
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+public interface CronService {
+
+    /**
+     * 根据 定制器 ID 查询定时器
+     *
+     * @param cronId 定时器 ID
+     * @return 返回定时器
+     */
+    Cron selectbyId(Long cronId);
+
+    /**
+     * 查询未删除的定时器
+     *
+     * @param deleted 是否删除
+     * @return 返回未删除的定时器
+     */
+    List<Cron> selectByDeleted(Boolean deleted);
+
+}
