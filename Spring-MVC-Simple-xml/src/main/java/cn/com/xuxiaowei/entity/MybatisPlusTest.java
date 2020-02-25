79484083698d304e298e09a62ba917c8cf32b2e9
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-02-25 20:59:26 +0800

    主键自增

--- Spring-MVC-Simple-xml/src/main/java/cn/com/xuxiaowei/entity/MybatisPlusTest.java
+++ Spring-MVC-Simple-xml/src/main/java/cn/com/xuxiaowei/entity/MybatisPlusTest.java
@@ -15,6 +15,7 @@
  */
 package cn.com.xuxiaowei.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
@@ -40,7 +41,7 @@ public class MybatisPlusTest implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @TableId("test_id")
+    @TableId(value = "test_id", type = IdType.AUTO)
     private Long testId;
 
     @TableField("test1")
