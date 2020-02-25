5c26428ccb814f1759f91f91b28ebf55a55257ef
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-02-25 21:45:39 +0800

    XmlRootElement

--- Spring-MVC-Simple-java/src/main/java/cn/com/xuxiaowei/entity/User.java
+++ Spring-MVC-Simple-java/src/main/java/cn/com/xuxiaowei/entity/User.java
@@ -15,17 +15,25 @@
  */
 package cn.com.xuxiaowei.entity;
 
-import lombok.Data;
+import lombok.*;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.io.Serializable;
 
 /**
  * 用户 实体类
+ * <p>
+ * 本实体类牵扯到返回 XML 类型的数据，需要在 Get 方法上使用注解 {@link XmlElement}，并且不能使用 {@link Getter}，
+ * 其中包含 {@link Getter} 的 {@link Data} 也不能使用
  *
  * @author xuxiaowei
  * @since 0.0.1
  */
-@Data
+@Setter
+@ToString
+@EqualsAndHashCode
+@XmlRootElement
 public class User implements Serializable {
 
     private static final long serialVersionUID = -1476784879155085411L;
@@ -51,5 +59,20 @@ public class User implements Serializable {
      * 密码
      */
     private String password;
+
+    @XmlElement
+    public Long getUserId() {
+        return userId;
+    }
+
+    @XmlElement
+    public String getUsername() {
+        return username;
+    }
+
+    @XmlElement
+    public String getPassword() {
+        return password;
+    }
 
 }
